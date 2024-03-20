52654af97e22a73cf41128833096e1633cfbe8ec
Mauri84360 <109274107+Mauri84360@users.noreply.github.com>
2024-03-20 01:31:14 -0600

    Delete src directory

--- src/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-export const Footer = () => { // con const y sin default
-    // el resto del código igual
-  };
-  
